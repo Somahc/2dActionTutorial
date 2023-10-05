c0f731a0e2486e2dac9d38ed6683d6cf952f3514
Soma Sekine <a8121151@aoyama.jp>
2023-10-05 12:55:43 -0400

    左右キーでキャラが動くように

---
+++ Assets/PlayerCtrl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb2d = GetComponent<Rigidbody2D>();
+        this.anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal"); // 左入力で-1、右入力で1、入力なしで0
+
+        anim.SetFloat("Speed", Mathf.Abs(x * speed));
+
+        rb2d.AddForce( Vector2.right * x * speed );
+
+    }
+}
