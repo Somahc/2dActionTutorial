ee9e2696abe4f6398eaae49f151418f9d1e0567a
Soma Sekine <a8121151@aoyama.jp>
2023-10-05 13:40:20 -0400

    キャラの左右を向く挙動追加

--- Assets/PlayerCtrl.cs
+++ Assets/PlayerCtrl.cs
@@ -7,12 +7,14 @@ public class PlayerCtrl : MonoBehaviour
     public float speed = 5f;
     private Rigidbody2D rb2d;
     private Animator anim;
+    private SpriteRenderer spRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         this.rb2d = GetComponent<Rigidbody2D>();
         this.anim = GetComponent<Animator>();
+        this.spRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,13 @@ public class PlayerCtrl : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal"); // 左入力で-1、右入力で1、入力なしで0
 
         anim.SetFloat("Speed", Mathf.Abs(x * speed));
+
+        // キャラの左右の向きを変える
+        if(x<0){
+            spRenderer.flipX = true;
+        }else if(x>0){
+            spRenderer.flipX = false;
+        }
 
         rb2d.AddForce( Vector2.right * x * speed );
 
